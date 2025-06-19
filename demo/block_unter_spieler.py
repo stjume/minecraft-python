572176ff0e83704ea243889f50576dcc83404f7c
nonchris <git@chris-ge.de>
2025-06-19 14:01:14 +0200

    update demo

--- demo/block_unter_spieler.py
+++ demo/block_unter_spieler.py
@@ -1,11 +1,14 @@
+import time
 import sk_minecraft
 
-sk_minecraft.verbinden("131.220.6.219", 12345)
+sk_minecraft.verbinden("10.151.9.209", 12345)
 
 print("verbunden")
 
-spieler = sk_minecraft.hole_spieler_koordinaten()
-print(spieler)
+while True:
+    spieler = sk_minecraft.hole_spieler_koordinaten()
+    print(spieler)
 
 
-sk_minecraft.setze_block(spieler.x, spieler.y-1, spieler.z, "stone")
+    sk_minecraft.setze_block(spieler.x, spieler.y-1, spieler.z, "stone")
+    # time.sleep(0.1)
