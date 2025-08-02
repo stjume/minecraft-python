98b7ca5c086d3ded3a4e7791f6e39a9fc2b6943c
Adrian Oeyen <oeyenadrian@gmail.com>
2025-08-02 21:03:02 +0200

    add a small stress_test demo

---
+++ demo/stress_test.py
@@ -0,0 +1,12 @@
+import sk_minecraft
+
+sk_minecraft.verbinden("localhost", 12345);
+
+spieler = sk_minecraft.hole_spieler()
+
+size = 10
+for x in range(size):
+    for y in range(size):
+            for z in range(size):
+                sk_minecraft.hole_spieler()
+                sk_minecraft.setze_block(spieler.x + x, spieler.y + y, spieler.z - size * 2 + z, sk_minecraft.MaterialSammlung.Stein)
