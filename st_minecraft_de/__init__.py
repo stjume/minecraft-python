522e2c1364d76a5e6bbc41b92098364de180802b
nonchris <git@chris-ge.de>
2025-12-20 21:55:44 +0100

    init

---
+++ st_minecraft_de/__init__.py
@@ -0,0 +1,14 @@
+""" st_minecraft package """
+
+from st_minecraft.en.core import connect  # noqa: unused-import
+from st_minecraft.main import *  # noqa: unused-import
+
+try:
+    from st_minecraft.entity import *  # noqa: unused-import
+    from st_minecraft.material import *  # noqa: unused-import
+
+except ModuleNotFoundError as e:
+    raise RuntimeError(
+        f"Es ist nicht möglich die auto-generierten Enums für Material und/oder Entity zu importieren."
+        f"Bitte überprüfe, ob diese vorliegen, falls nicht siehe bitte ressourcen/generiere_enums.py im git repo."
+    ) from e
