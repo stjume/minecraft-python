19aef3c9a98ffc9b48a3275bf5b9fc1f10c82679
nonchris <git@chris-ge.de>
2026-05-11 01:14:15 +0200

    move all metadata to __init__ and delete __about__.py, update author field

--- st_minecraft/__init__.py
+++ st_minecraft/__init__.py
@@ -1,1 +1,21 @@
 """ st_minecraft package """
+
+__author__ = "Chris Geron, Adrian Oeyen & sk stiftung jugend und medien der Sparkasse KölnBonn"
+__email__ = "git@chris-ge.de"
+
+from typing import Literal
+from typing import NamedTuple
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+__version__ = "1.1.1"
+version = VersionInfo(major=1, minor=1, micro=1, releaselevel="final", serial=0)
+
+del NamedTuple, Literal
