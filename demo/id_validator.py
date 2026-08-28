b5a5f394850ef46b15dc83c987bf89364687312b
nonchris <git@chris-ge.de>
2026-08-28 18:20:28 +0200

    update validator script to work on enums

--- demo/id_validator.py
+++ demo/id_validator.py
@@ -1,8 +1,7 @@
-import csv
+import st_minecraft.de as mc
+from st_minecraft.de.material import MaterialSammlung
 
-import st_minecraft.de as st_minecraft
-
-st_minecraft.verbinden("localhost")
+mc.verbinden("192.168.3.102")
 
 # with open("blocks_items.csv") as f:
 #     names = csv.reader(f)
@@ -11,9 +10,20 @@ st_minecraft.verbinden("localhost")
 #         if result == "Yes":
 #             print(",".join(row))
 
-with open("entities.csv") as f:
-    names = csv.reader(f)
-    for row in names:
-        result = st_minecraft._validiere_id(row[1], "ENTITY")
-        if result == "Yes":
-            print(",".join(row))
+ms = MaterialSammlung._value2member_map_
+
+print(ms)
+
+p = mc.hole_spieler()
+
+for e, v in ms.items():
+    mc.setze_block(p.x, p.y, p.z, v)
+    print(v)
+
+print("Done")
+
+mc.gebe_item(p, MaterialSammlung.Totem_der_Unsterblichkeit, 4, None, 2)
+
+i = mc.hole_inventar(p)
+
+print(i[1])
